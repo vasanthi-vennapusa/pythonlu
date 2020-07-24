af47f3306b8ff14f3d57b1e7d2e6381317968a10
vasanthi-vennapusa <66934745+vasanthi-vennapusa@users.noreply.github.com>
2020-07-24 11:15:46 +0530

    first commit

---
+++ DAY4ASSIGNMENT.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Day 4 assignment
+# find all occurence of substring in the given string?
+
+# In[5]:
+
+
+str1="what we think we become; We are python programmer"
+str1.find("we")
+
+
+# In[6]:
+
+
+str1.rfind("we")
+
+
+# In[7]:
+
+
+str1.count("we")
+
+
+# In[8]:
+
+
+str1.find("what")
+
+
+# In[9]:
+
+
+str1.rfind("what")
+
+
+# In[10]:
+
+
+str1.count("what")
+
+
+# In[11]:
+
+
+str1.index("we")
+
+
+# In[12]:
+
+
+str1="what we think we become; We are python programmer"
+str1.rfind("we")
+str1.find("what")
+str1.rfind("what")
+print(str1)
+
+
+# # lower and upper functions
+
+# In[13]:
+
+
+str1="python"
+str2="programmer"
+str3="letsupgrade"
+str4="batchsix"
+str1.upper()
+
+
+# In[14]:
+
+
+str2.lower()
+
+
+# In[15]:
+
+
+str3.lower()
+
+
+# In[16]:
+
+
+
+str4.lower()
+
+
+# In[17]:
+
+
+
+str1.isupper()
+
